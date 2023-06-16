91111899a3ccfad75f899554aa366a136da46303
Samuel Olvera <jsamline@gmail.com>
2023-06-16 13:54:30 -0600

    jsx files

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 /* Css */
-import { breakpoints } from './components/Css/breakpoints'
 
 /* ./ */
-import { Box } from './components/Box'
+import { Box } from './components/Box/index.js'
 import { Flex } from './components/Flex'
 import { Grid } from './components/Grid'
 import { Ly } from './components/Ly'
 import { Track } from './components/Track'
 
 export { Box, Flex, Grid, Ly, Track }
-export { breakpoints }
