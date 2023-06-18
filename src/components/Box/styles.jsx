aef11d2564dd09896ae40ce250cee05563655fd2
Samuel Olvera <jsamline@gmail.com>
2023-06-18 13:31:57 -0600

    test

--- src/components/Box/styles.jsx
+++ src/components/Box/styles.jsx
@@ -1,8 +1,9 @@
 /* Packages */
+import { _interop } from '@honeybee-js/js-interop'
 import styled, { css } from 'styled-components'
 import { compose, layout, position, space } from 'styled-system'
 
-export const Box = styled.div`
+export const Box = _interop(styled).div`
   display: block;
   margin: 0 auto;
   padding: 0rem 0.625rem;
